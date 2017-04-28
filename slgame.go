636a3fcb122773ad2b0fc9ad8cb1c88c86fcf86b
b6luong <b6luong@scs.ryerson.ca>
2017-04-28 10:06:48 -0400

    initial version of whole game

--- slgame.go
+++ slgame.go
@@ -3,9 +3,12 @@ package main
 import (
 	"Snakes-and-Ladders/slgame"
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -13,8 +16,8 @@ func check(e error) {
 		panic(e)
 	}
 }
-func main() {
 
+func runGameUsingStdIn() {
 	reader := bufio.NewReader(os.Stdin)
 	var input string
 	config := ""
@@ -27,6 +30,100 @@ func main() {
 		check(err)
 	}
 	game := slgame.ReadFrom(config)
-	//fmt.Println(game)
 	fmt.Println(slgame.Print(game))
+}
+
+func runGameUsingJsonIO(infile string, outfile string) {
+	jsonRawData, err := ioutil.ReadFile(infile)
+	check(err)
+
+	var jsonData map[string]interface{}
+	parseError := json.Unmarshal(jsonRawData, &jsonData)
+	check(parseError)
+
+	author := jsonData["author"].(string)
+	fmt.Println("author:", author)
+
+	jsonOutData := make(map[string]interface{})
+	jsonOutData["requestedBy"] = author
+
+	resultSet := []map[string]string{}
+
+	for _, i := range jsonData["configurations"].([]interface{}) {
+		obj := i.(map[string]interface{})
+		title := obj["title"].(string)
+		config := obj["commands"].(string)
+		result := make(map[string]string)
+		result["title"] = title
+		result["result"] = slgame.Print(slgame.ReadFrom(config))
+		resultSet = append(resultSet, result)
+		//fmt.Println("title:", title)
+		//fmt.Println("config:", config)
+		//fmt.Println(slgame.Print(slgame.ReadFrom(config)))
+	}
+
+	jsonOutData["resultSet"] = resultSet
+	jsonOutRaw, marshErr := json.Marshal(jsonOutData)
+	check(marshErr)
+	writeErr := ioutil.WriteFile(outfile, jsonOutRaw, 0644)
+	check(writeErr)
+}
+
+func runGameUsingJsonIn(infile string) {
+	jsonRawData, err := ioutil.ReadFile(infile)
+	check(err)
+
+	var jsonData map[string]interface{}
+	parseError := json.Unmarshal(jsonRawData, &jsonData)
+	check(parseError)
+
+	author := jsonData["author"].(string)
+	fmt.Println("author:", author)
+
+	for _, i := range jsonData["configurations"].([]interface{}) {
+		obj := i.(map[string]interface{})
+		title := obj["title"].(string)
+		config := obj["commands"].(string)
+		fmt.Println("title:", title)
+		fmt.Println("config:", config)
+		fmt.Println(slgame.Print(slgame.ReadFrom(config)))
+	}
+}
+
+func main() {
+
+	jsonIn := false
+	jsonInFile := ""
+	jsonOut := false
+	jsonOutFile := ""
+	pwd, pathErr := os.Getwd()
+	check(pathErr)
+
+	fmt.Println(os.Args)
+	fmt.Println(pwd + "/")
+
+	for _, arg := range os.Args {
+		if strings.Contains(arg, "-json-in=") {
+			jsonIn = true
+			jsonInFile = pwd + "/" + arg[9:]
+		} else if strings.Contains(arg, "-json-out=") {
+			jsonOut = true
+			jsonOutFile = pwd + "/" + arg[10:]
+		}
+	}
+
+	fmt.Println("jsonIn:", jsonIn)
+	fmt.Println("jsonInFile:", jsonInFile)
+	fmt.Println("jsonOut:", jsonOut)
+	fmt.Println("jsonOutFile:", jsonOutFile)
+
+	if jsonIn && jsonOut {
+		runGameUsingJsonIO(jsonInFile, jsonOutFile)
+	} else if !jsonIn && jsonOut {
+		//runGameUsingJsonOut(jsonOutFile)
+	} else if jsonIn && !jsonOut {
+		runGameUsingJsonIn(jsonInFile)
+	} else {
+		runGameUsingStdIn()
+	}
 }
